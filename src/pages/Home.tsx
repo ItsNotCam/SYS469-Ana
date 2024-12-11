1c12ef0b908724270f70a06e9649de0799b5b2cc
ItsNotCam <camallanyoung@gmail.com>
2024-12-10 23:40:33 -0500

    and1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import NavMini from '../components/nav-mini';
 
 const Home: React.FC = () => (
 	<div className='flex h-screen overflow-hidden w-full'>
-		<img src={model3} className="-z-[1] absolute left-1/2 -translate-x-2/3 top-[35%] h-[850px]"/>
+		<img src={model3} className="object-cover -z-[1] h-[850px] mr-auto mt-auto ml-12"/>
 		<div className="absolute bottom-[7rem] left-[20%] flex flex-row gap-12">
 			<SpotifyMini 
 				icon={albumIcon}
